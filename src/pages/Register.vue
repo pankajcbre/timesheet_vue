57dcea716d282bcc62ce513b8f3926cef024295f
pankajcbre <50093299+pankajcbre@users.noreply.github.com>
2020-05-12 17:16:17 +0530

    added dashboard and register

---
+++ src/pages/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <RegisterC />
+    <p>sfer</p>
+  </div>
+</template>
+
+<script>
+import RegisterC from "@/components/RegisterC.vue";
+export default {
+  name: "Register",
+  components: {
+    RegisterC
+  }
+};
+</script>
