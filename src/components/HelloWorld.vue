0432a75de96fd159372c2b381f34e799d4da3e7f
pankajcbre <50093299+pankajcbre@users.noreply.github.com>
2020-05-12 15:36:13 +0530

    hello

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">Hi This is sample page</v-col>
+      <h1>jyoti sample</h1>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({})
+};
+</script>
