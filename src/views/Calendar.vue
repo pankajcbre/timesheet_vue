d74aea003801607e5d13b6ab07afe54215e5b00b
pankajkumar143 <pankajkumar143@gmail.com>
2020-05-19 16:33:09 +0530

    git merge

---
+++ src/views/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class>
+    <CalendarC />
+  </div>
+</template>
+
+<script>
+import CalendarC from "../components/CalendarC";
+
+export default {
+  name: "Calendar",
+  components: {
+    CalendarC
+  }
+};
+</script>
+<style>
+</style>
