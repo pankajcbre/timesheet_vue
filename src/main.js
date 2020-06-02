ade90bc3b837aea945a75ba829ab917fa7a60e27
pankajkumar143 <pankajkumar143@gmail.com>
2020-06-02 09:46:34 +0530

    calendar

--- src/main.js
+++ src/main.js
@@ -2,10 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import vuetify from './plugins/vuetify'
+import VueTextareaAutosize from 'vue-textarea-autosize'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 import './assets/scss/style.scss'
 
+Vue.use(VueTextareaAutosize);
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDPUuad4XD4ziEBL55REFgjvf639T-syS4",
+  authDomain: "leaveplanner-df9b8.firebaseapp.com",
+  databaseURL: "https://leaveplanner-df9b8.firebaseio.com",
+  projectId: "leaveplanner-df9b8",
+  storageBucket: "leaveplanner-df9b8.appspot.com",
+  messagingSenderId: "917597276289",
+  appId: "1:917597276289:web:764353f23bd9be12e82580"
+
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   router,
--- src/shared/rightSidebar.vue
+++ src/shared/rightSidebar.vue
@@ -1,84 +1,120 @@
-<template>   
-    <div>
-        <div class="rightSideHead">
-            <h2>Activity Feed</h2>            
-        </div><!--//rightSideHead-->
-
-        <section class="activitySeciton">
-            <h3><span>10 May 2020</span></h3>
-
-            <v-timeline dense clipped>
-                <v-timeline-item color="grey" icon-color="grey lighten-2" medium v-for="(activity, i) in activities" :Key="i">
-                    <template v-slot:icon>
-                        <span>JL</span>
-                    </template>
-                    <v-row>
-                        <v-col cols="12">
-                            <h4>{{activity.activityTitle}}</h4>
-                            <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
-                            <p>{{activity.time}}</p>
-                        </v-col>
-                    </v-row>
-                </v-timeline-item>             
-            </v-timeline>
-        </section>
-
-        
-
-        <section class="activitySeciton">
-            <h3><span>09 May 2020</span></h3>
-
-            <v-timeline dense clipped>
-                <v-timeline-item color="grey" icon-color="grey lighten-2" medium v-for="(activity, i) in activities" :Key="i">
-                    <template v-slot:icon>
-                        <span>JL</span>
-                    </template>
-                    <v-row>
-                        <v-col cols="12">
-                            <h4>{{activity.activityTitle}}</h4>
-                            <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
-                            <p>{{activity.time}}</p>
-                        </v-col>
-                    </v-row>
-                </v-timeline-item>             
-            </v-timeline>
-        </section>
-
-        
-
-        <section class="activitySeciton">
-            <h3><span>08 May 2020</span></h3>
-
-            <v-timeline dense clipped>
-                <v-timeline-item color="grey" icon-color="grey lighten-2" medium v-for="(activity, i) in activities" :Key="i">
-                    <template v-slot:icon>
-                        <span>JL</span>
-                    </template>
-                    <v-row>
-                        <v-col cols="12">
-                            <h4>{{activity.activityTitle}}</h4>
-                            <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
-                            <p>{{activity.time}}</p>
-                        </v-col>
-                    </v-row>
-                </v-timeline-item>             
-            </v-timeline>
-        </section>
-
+<template>
+  <div>
+    <div class="rightSideHead">
+      <h2>Activity Feed</h2>
     </div>
+    <!--//rightSideHead-->
+
+    <section class="activitySeciton">
+      <h3>
+        <span>10 May 2020</span>
+      </h3>
+
+      <v-timeline dense clipped>
+        <v-timeline-item
+          color="grey"
+          icon-color="grey lighten-2"
+          medium
+          v-for="(activity, index) in activities"
+          :Key="index"
+        >
+          <template v-slot:icon>
+            <span>JL</span>
+          </template>
+          <v-row>
+            <v-col cols="12">
+              <h4>{{activity.activityTitle}}</h4>
+              <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
+              <p>{{activity.time}}</p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
+    </section>
+
+    <section class="activitySeciton">
+      <h3>
+        <span>09 May 2020</span>
+      </h3>
+
+      <v-timeline dense clipped>
+        <v-timeline-item
+          color="grey"
+          icon-color="grey lighten-2"
+          medium
+          v-for="(activity, index) in activities"
+          :Key="index"
+        >
+          <template v-slot:icon>
+            <span>JL</span>
+          </template>
+          <v-row>
+            <v-col cols="12">
+              <h4>{{activity.activityTitle}}</h4>
+              <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
+              <p>{{activity.time}}</p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
+    </section>
+
+    <section class="activitySeciton">
+      <h3>
+        <span>08 May 2020</span>
+      </h3>
+
+      <v-timeline dense clipped>
+        <v-timeline-item
+          color="grey"
+          icon-color="grey lighten-2"
+          medium
+          v-for="(activity, index) in activities"
+          :Key="index"
+        >
+          <template v-slot:icon>
+            <span>JL</span>
+          </template>
+          <v-row>
+            <v-col cols="12">
+              <h4>{{activity.activityTitle}}</h4>
+              <h5 v-if="activity.projectName">{{activity.projectName}}</h5>
+              <p>{{activity.time}}</p>
+            </v-col>
+          </v-row>
+        </v-timeline-item>
+      </v-timeline>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default{
-        data(){
-            return{
-                activities:[
-                    {activityTitle:'You added an Admin Task to your Time Sheet', projectName:'Project Name', time:'9:00 AM' },
-                    {activityTitle:'New task assigned to you.', projectName:'Project Name', time:'9:00 AM' },
-                    {activityTitle:'Steve approved your leave plan.', projectName:'', time:'9:00 AM' },
-                    {activityTitle:'David accepted your task transfer request.', projectName:'Project Name', time:'9:00 AM' }
-                ]
-            }
+export default {
+  data() {
+    return {
+      activities: [
+        {
+          activityTitle: "You added an Admin Task to your Time Sheet",
+          projectName: "Project Name",
+          time: "9:00 AM"
+        },
+        {
+          activityTitle: "New task assigned to you.",
+          projectName: "Project Name",
+          time: "9:00 AM"
+        },
+        {
+          activityTitle: "Steve approved your leave plan.",
+          projectName: "",
+          time: "9:00 AM"
+        },
+        {
+          activityTitle: "David accepted your task transfer request.",
+          projectName: "Project Name",
+          time: "9:00 AM"
         }
-    }
+      ]
+    };
+  }
+};
 </script>
